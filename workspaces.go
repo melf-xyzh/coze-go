6f7482acf448a4f8dd40bddf8de04868312c6c4d
chyroc <chyroc@qq.com>
2025-06-20 23:35:42 +0800

    feat: Update Workspaces.List api (#61)

--- workspaces.go
+++ workspaces.go
@@ -6,6 +6,9 @@ import (
 	"strconv"
 )
 
+// 查看空间列表
+//
+// docs: https://www.coze.cn/open/docs/developer_guides/list_workspace
 func (r *workspace) List(ctx context.Context, req *ListWorkspaceReq) (NumberPaged[Workspace], error) {
 	if req.PageSize == 0 {
 		req.PageSize = 20
@@ -17,10 +20,7 @@ func (r *workspace) List(ctx context.Context, req *ListWorkspaceReq) (NumberPage
 		func(request *pageRequest) (*pageResponse[Workspace], error) {
 			uri := "/v1/workspaces"
 			resp := &listWorkspaceResp{}
-			err := r.core.Request(ctx, http.MethodGet, uri, nil, resp,
-				withHTTPQuery("page_num", strconv.Itoa(request.PageNum)),
-				withHTTPQuery("page_size", strconv.Itoa(request.PageSize)))
-			if err != nil {
+			if err := r.core.Request(ctx, http.MethodGet, uri, nil, resp, req.toReq(request)...); err != nil {
 				return nil, err
 			}
 			return &pageResponse[Workspace]{
@@ -32,18 +32,13 @@ func (r *workspace) List(ctx context.Context, req *ListWorkspaceReq) (NumberPage
 		}, req.PageSize, req.PageNum)
 }
 
-type workspace struct {
-	core *core
-}
-
-func newWorkspace(core *core) *workspace {
-	return &workspace{core: core}
-}
-
 // ListWorkspaceReq represents the request parameters for listing workspaces
 type ListWorkspaceReq struct {
-	PageNum  int `json:"page_num"`
-	PageSize int `json:"page_size"`
+	PageNum       int     `json:"page_num"`
+	PageSize      int     `json:"page_size"`
+	EnterpriseID  *string `json:"enterprise_id,omitempty"`
+	UserID        *string `json:"user_id,omitempty"`
+	CozeAccountID *string `json:"coze_account_id,omitempty"`
 }
 
 func NewListWorkspaceReq() *ListWorkspaceReq {
@@ -73,6 +68,7 @@ type Workspace struct {
 	IconUrl       string            `json:"icon_url"`
 	RoleType      WorkspaceRoleType `json:"role_type"`
 	WorkspaceType WorkspaceType     `json:"workspace_type"`
+	EnterpriseID  string            `json:"enterprise_id"`
 }
 
 // WorkspaceRoleType represents the workspace role type
@@ -91,3 +87,28 @@ const (
 	WorkspaceTypePersonal WorkspaceType = "personal"
 	WorkspaceTypeTeam     WorkspaceType = "team"
 )
+
+func (r ListWorkspaceReq) toReq(request *pageRequest) []RequestOption {
+	res := []RequestOption{
+		withHTTPQuery("page_num", strconv.Itoa(request.PageNum)),
+		withHTTPQuery("page_size", strconv.Itoa(request.PageSize)),
+	}
+	if r.EnterpriseID != nil {
+		res = append(res, withHTTPQuery("enterprise_id", *r.EnterpriseID))
+	}
+	if r.UserID != nil {
+		res = append(res, withHTTPQuery("user_id", *r.UserID))
+	}
+	if r.CozeAccountID != nil {
+		res = append(res, withHTTPQuery("coze_account_id", *r.CozeAccountID))
+	}
+	return res
+}
+
+type workspace struct {
+	core *core
+}
+
+func newWorkspace(core *core) *workspace {
+	return &workspace{core: core}
+}
